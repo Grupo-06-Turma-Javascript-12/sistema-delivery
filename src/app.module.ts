22b10b0971656039a93e8c352a13a74f0313784c
Thays Peixoto <thabysilva12@gmail.com>
2026-01-28 16:00:53 -0300

    realiza atualizações para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ProdutoModule } from './produto/produto.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
