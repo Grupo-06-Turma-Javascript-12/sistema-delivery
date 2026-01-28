e8530fa5df52c9ad5be928b17a8c4f8d43635917
Thays Peixoto <thabysilva12@gmail.com>
2026-01-28 15:49:24 -0300

    corrige main

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,20 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00';
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  app.enableCors();
   
   const config = new DocumentBuilder()
-      .setTitle('API Delivery')
-      .setDescription('API para gerenciamento de produtos, categorias e usuarios')
-      .setVersion('1.0')
-      .build();
-
+  .setTitle('API Delivery')
+  .setDescription('API para gerenciamento de produtos, categorias e usuarios')
+  .setVersion('1.0')
+  .build();
+  
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/swagger', app, document);
+  
+    process.env.TZ = '-03:00';
+    app.useGlobalPipes(new ValidationPipe());
 
-
+    app.enableCors();
   await app.listen(process.env.PORT ?? 4000);
 }
 void bootstrap();
