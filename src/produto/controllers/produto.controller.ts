f8fe7e5fe455d6e289364eb0bd65a600c69df728
dapperdile <rafinhawc@hotmail.com>
2026-01-28 14:44:21 -0300

    Adicionado Produto para a main

---
+++ src/produto/controllers/produto.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../Auth/guard/jwt-auth.guard';
+import { Produto } from '../entities/produto.entity';
+import { ProdutoService } from '../services/produto.service';
+
+@ApiTags('produto')
+@UseGuards(JwtAuthGuard)
+@Controller('/produto')
+@ApiBearerAuth()
+export class ProdutoController {
+  constructor(private readonly produtoService: ProdutoService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Produto[]> {
+    return this.produtoService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Produto> {
+    return this.produtoService.findById(id);
+  }
+
+  @Get('/nome/:nome')
+  @HttpCode(HttpStatus.OK)
+  findByTitulo(@Param('nome') nome: string): Promise<Produto[]> {
+    return this.produtoService.findByTitulo(nome);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() produto: Produto): Promise<Produto> {
+    return this.produtoService.create(produto);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() produto: Produto): Promise<Produto> {
+    return this.produtoService.update(produto);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.produtoService.delete(id);
+  }
+}
