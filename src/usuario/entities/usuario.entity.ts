56bce89bdb80bb6950cb4d2bec732893413f6611
unknown <deboraccamposs@gmail.com>
2026-01-28 11:28:32 -0300

    Parte do Usuário adicionada

---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Produto } from '../../produto/entities/produto.entity';
+
+@Entity({ name: 'tb_usuario' })
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  nome: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  usuario: string;
+
+  @MinLength(8)
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  senha: string;
+
+  @Column({ length: 5000 })
+  foto: string;
+
+  @OneToMany(() => Produto, (produto) => produto.usuario)
+  produto: Produto[];
+}
